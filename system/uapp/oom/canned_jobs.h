subhambapna/magenta

WIP(oom): oom-ranker; TODO: make it loop, make it a device, add tests
Change-Id: I870e76029d094171a3d39fc4d0cb44c66f12df40

system/uapp/oom/canned_jobs.h
@@ -1,0 +1,34 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include <mx/job.h>
+#include <mxtl/array.h>
+#include <mxtl/macros.h>
+#include <mxtl/unique_ptr.h>
+
+// Holds onto job handles and keeps them alive.
+// TODO: Consider making this object<T>-templated and move into mx.
+class JobStack : private mxtl::Array<mx::job> {
+public:
+    static JobStack *Create(size_t count);
+
+    // Swaps the job onto the stack.
+    void push(mx::job* job);
+
+    // Returns the most-recently-pushed job.
+    mx::job& top() const;
+
+private:
+    JobStack(mx::job* array, size_t count);
+    DISALLOW_COPY_ASSIGN_AND_MOVE(JobStack);
+
+    size_t next_;
+};
+
+// Creates a canned tree of jobs under the specified root job.
+// Does not create any processes.
+mx_status_t create_test_jobs_under(
+    mx_handle_t root, mxtl::unique_ptr<JobStack>* jobs);

system/uapp/oom/fake_syscalls.cpp
@@ -1,0 +1,130 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// Fake syscalls for testing.
+
+#include "fake_syscalls.h"
+
+#include <magenta/status.h>
+#include <magenta/syscalls.h>
+#include <magenta/syscalls/object.h>
+#include <mxalloc/new.h>
+#include <mxtl/intrusive_double_list.h>
+#include <mxtl/unique_ptr.h>
+
+#include <string.h>
+#include <inttypes.h>
+
+#include "macros.h"
+
+using mxtl::unique_ptr;
+
+namespace {
+
+mx_status_t get_handle_koid(mx_handle_t handle, mx_koid_t* koid) {
+    mx_info_handle_basic_t info;
+    RETURN_IF_ERROR(mx_object_get_info(
+        handle, MX_INFO_HANDLE_BASIC, &info, sizeof(info), nullptr, nullptr));
+    *koid = info.koid;
+    return MX_OK;
+}
+
+class RankedJob
+    : public mxtl::DoublyLinkedListable<mxtl::unique_ptr<RankedJob>> {
+public:
+    static mx_status_t Create(mx_handle_t handle, unique_ptr<RankedJob>* out) {
+        mx_koid_t koid;
+        RETURN_IF_ERROR(get_handle_koid(handle, &koid));
+
+        char name[MX_MAX_NAME_LEN];
+        RETURN_IF_ERROR(
+            mx_object_get_property(handle, MX_PROP_NAME, name, sizeof(name)));
+
+        AllocChecker ac;
+        out->reset(new (&ac) RankedJob(koid, name));
+        if (!ac.check()) {
+            return MX_ERR_NO_MEMORY;
+        }
+        return MX_OK;
+    }
+
+    mx_koid_t koid() const { return koid_; }
+    const char* name() const { return name_; }
+
+    // Type of linked lists of this class.
+    using List = mxtl::DoublyLinkedList<mxtl::unique_ptr<RankedJob>>;
+
+private:
+    RankedJob(mx_koid_t koid, const char* name)
+        : koid_(koid) {
+        strlcpy(const_cast<char*>(name_), name, sizeof(name_));
+    }
+
+    const char name_[MX_MAX_NAME_LEN] = {};
+    const mx_koid_t koid_;
+};
+
+RankedJob::List ranked_jobs;
+
+RankedJob* get_ranked_job_by_koid(mx_koid_t koid) {
+    for (auto& job : ranked_jobs) {
+        if (job.koid() == koid) {
+            return &job;
+        }
+    }
+    return nullptr;
+}
+
+} // namespace
+
+mx_status_t _fake_job_set_relative_importance(
+    mx_handle_t root_resource,
+    mx_handle_t job, mx_handle_t less_important_job) {
+
+    // Make sure the root resource looks legit.
+    mx_info_handle_basic_t info;
+    RETURN_IF_ERROR(mx_object_get_info(root_resource, MX_INFO_HANDLE_BASIC,
+                                       &info, sizeof(info), nullptr, nullptr));
+    if (info.type != MX_OBJ_TYPE_RESOURCE) {
+        return MX_ERR_WRONG_TYPE;
+    }
+
+    unique_ptr<RankedJob> rjob;
+    {
+        mx_koid_t koid;
+        RETURN_IF_ERROR(get_handle_koid(job, &koid));
+        RankedJob* j = get_ranked_job_by_koid(koid);
+        if (j == nullptr) {
+            RETURN_IF_ERROR(RankedJob::Create(job, &rjob));
+        } else {
+            rjob.reset(ranked_jobs.erase(*j).release());
+        }
+    }
+    fflush(stdout);
+    MX_ASSERT(!rjob->InContainer());
+
+    if (less_important_job == MX_HANDLE_INVALID) {
+        // Make this the least important.
+        ranked_jobs.push_front(mxtl::move(rjob));
+    } else {
+        // Insert rjob just after less_important_job.
+        mx_koid_t koid;
+        RETURN_IF_ERROR(get_handle_koid(less_important_job, &koid));
+        RankedJob* li_job = get_ranked_job_by_koid(koid);
+        // Simplification: less_important_job must exist already. The real
+        // syscall wouldn't have this restriction.
+        MX_DEBUG_ASSERT(li_job != nullptr);
+        ranked_jobs.insert_after(
+            ranked_jobs.make_iterator(*li_job), mxtl::move(rjob));
+    }
+    return MX_OK;
+}
+
+void dump_importance_list() {
+    printf("Least important:\n");
+    for (const auto& rj : ranked_jobs) {
+        printf("- k:%" PRIu64 " [%-*s]\n",
+               rj.koid(), MX_MAX_NAME_LEN, rj.name());
+    }
+}

system/uapp/oom/job.h
@@ -1,0 +1,100 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include <magenta/types.h>
+#include <mxtl/intrusive_double_list.h>
+#include <mxtl/unique_ptr.h>
+
+// A running Magenta job, with an open handle and cached properties.
+//
+// Defines a globally-sortable "importance key" that can be used to
+// rank all jobs by relative importance.
+//
+// Importance keys have the form:
+//
+//   <capped-importance> ':' <importance-chain> '~'
+//
+// <capped-importance> is the Job's capped_importance() as two hex digits.
+// <importance-chain> is the concatenation of the importance() values of a
+// Job's ancestors and itself, with the root-most Job's importance first.
+//
+// This produces a key that, when sorted using strcmp(), will cluster Jobs
+// with the same capped_importance(), then break ties using a Job's
+// ancestors' importances. The trailing '~' causes shorter strings to sort
+// higher than longer strings, ensuring that parents are more important
+// than their children.
+//
+// Example values:
+//
+//     32:c8c8c896c83264~ << A deep BACKGROUND job, capped to DISPOSABLE
+//     32:c8c8c896c832~
+//     32:c8c8c896c89632~
+//     64:c8c8c896c86464~
+//     64:c8c8c896c864c8~
+//     96:c8c8c896c8~     << A CRITICAL job, capped to FOREGROUND
+//     96:c8c8c896~       << The previous job's FOREGROUND parent
+//     96:c8c8c8c896~
+//     c8:c8c8~
+//     c8:c8~
+//
+// TODO(dbort): Consider using a similarly-structured uint64_t instead of a
+// string.
+class Job : public mxtl::DoublyLinkedListable<mxtl::unique_ptr<Job>> {
+public:
+    using importance_t = uint32_t;
+
+    // Takes ownership of |handle|, even on failure. To aid testability, this
+    // class must not perform any syscalls on |handle| except mx_handle_close.
+    static mx_status_t Create(
+        mx_koid_t koid, mx_handle_t handle,
+        const char* name, importance_t importance,
+        const Job* parent, mxtl::unique_ptr<Job>* out);
+
+    ~Job();
+
+    mx_koid_t koid() const { return koid_; }
+    const char* name() const { return name_.get(); }
+
+    // The importance returned by MX_PROP_JOB_IMPORTANCE.
+    importance_t importance() const { return importance_; }
+
+    // Like importance(), but capped to be no more important
+    // than any ancestor.
+    importance_t capped_importance() const {
+        return capped_importance_;
+    }
+
+    // An opaque, strcmp()-comparable string for sorting Jobs by their global
+    // relative importance. Higher importance will sort higher.
+    // See the class comment for a description of the keys.
+    const char* importance_key() const { return importance_key_.get(); }
+
+    // Returns the handle but maintains ownership.
+    mx_handle_t handle() const { return handle_; }
+
+    // Type for linked lists of this class.
+    using List = mxtl::DoublyLinkedList<mxtl::unique_ptr<Job>>;
+
+private:
+    Job(mx_koid_t koid, mx_handle_t handle, mxtl::unique_ptr<char[]> name,
+        importance_t importance, importance_t capped_importance,
+        mxtl::unique_ptr<char[]> importance_key);
+
+    const mx_koid_t koid_;
+
+    // An open handle to the underlying job object, or MX_HANDLE_INVALID.
+    // Closed when this object is destroyed.
+    const mx_handle_t handle_;
+
+    const mxtl::unique_ptr<char[]> name_;
+    const importance_t importance_;
+    const importance_t capped_importance_;
+    const mxtl::unique_ptr<char[]> importance_key_;
+};
+
+// Reorders |jobs| so entries are ordered by importance_key(), with
+// the least-important job first.
+void sort_jobs_by_importance_key(Job::List *jobs);

system/uapp/oom/macros.h
@@ -1,0 +1,19 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include <magenta/status.h>
+#include <stdio.h>
+
+#define RETURN_IF_ERROR(x)                                             \
+    do {                                                               \
+        mx_status_t TRY_status__ = (x);                                \
+        if (TRY_status__ != MX_OK) {                                   \
+            printf("%s:%d: %s failed: %s (%d)\n",                      \
+                    __func__, __LINE__, #x,                            \
+                    mx_status_get_string(TRY_status__), TRY_status__); \
+            return TRY_status__;                                       \
+        }                                                              \
+    } while (0)

system/uapp/oom/resources.h
@@ -1,0 +1,17 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include <magenta/compiler.h>
+#include <magenta/types.h>
+
+__BEGIN_CDECLS
+
+// Returns a new handle to the root resource, which the caller
+// is responsible for closing.
+// See docs/objects/resource.md
+mx_status_t get_root_resource(mx_handle_t* root_resource);
+
+__END_CDECLS

system/uapp/oom/rules.mk
@@ -1,0 +1,35 @@
+# Copyright 2017 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+MODULE := $(LOCAL_DIR).ranker
+
+MODULE_NAME := oom-ranker
+
+MODULE_TYPE := userapp
+
+MODULE_SRCS += \
+    $(LOCAL_DIR)/canned_jobs.cpp \
+    $(LOCAL_DIR)/job.cpp \
+    $(LOCAL_DIR)/ranker.cpp \
+    $(LOCAL_DIR)/resources.c
+
+# Tests
+MODULE_SRCS += \
+    $(LOCAL_DIR)/fake_syscalls.cpp
+
+MODULE_STATIC_LIBS := \
+    system/ulib/task-utils \
+    system/ulib/mxalloc \
+    system/ulib/mxcpp \
+    system/ulib/mx \
+    system/ulib/mxtl
+
+MODULE_LIBS := \
+    system/ulib/mxio \
+    system/ulib/magenta \
+    system/ulib/c
+
+include make/module.mk

pandarong/zircon

system/uapp/oom/canned_jobs.h
@@ -1,0 +1,34 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include <zx/job.h>
+#include <fbl/array.h>
+#include <fbl/macros.h>
+#include <fbl/unique_ptr.h>
+
+// Holds onto job handles and keeps them alive.
+// TODO: Consider making this object<T>-templated and move into zx.
+class JobStack : private fbl::Array<zx::job> {
+public:
+    static JobStack *Create(size_t count);
+
+    // Swaps the job onto the stack.
+    void push(zx::job* job);
+
+    // Returns the most-recently-pushed job.
+    zx::job& top() const;
+
+private:
+    JobStack(zx::job* array, size_t count);
+    DISALLOW_COPY_ASSIGN_AND_MOVE(JobStack);
+
+    size_t next_;
+};
+
+// Creates a canned tree of jobs under the specified root job.
+// Does not create any processes.
+zx_status_t create_test_jobs_under(
+    zx_handle_t root, fbl::unique_ptr<JobStack>* jobs);

system/uapp/oom/fake_syscalls.cpp
@@ -1,0 +1,130 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// Fake syscalls for testing.
+
+#include "fake_syscalls.h"
+
+#include <string.h>
+#include <inttypes.h>
+
+#include <fbl/intrusive_double_list.h>
+#include <fbl/unique_ptr.h>
+#include <zircon/status.h>
+#include <zircon/syscalls.h>
+#include <zircon/syscalls/object.h>
+#include <zxcpp/new.h>
+
+#include "macros.h"
+
+using fbl::unique_ptr;
+
+namespace {
+
+zx_status_t get_handle_koid(zx_handle_t handle, zx_koid_t* koid) {
+    zx_info_handle_basic_t info;
+    RETURN_IF_ERROR(zx_object_get_info(
+        handle, ZX_INFO_HANDLE_BASIC, &info, sizeof(info), nullptr, nullptr));
+    *koid = info.koid;
+    return ZX_OK;
+}
+
+class RankedJob
+    : public fbl::DoublyLinkedListable<fbl::unique_ptr<RankedJob>> {
+public:
+    static zx_status_t Create(zx_handle_t handle, unique_ptr<RankedJob>* out) {
+        zx_koid_t koid;
+        RETURN_IF_ERROR(get_handle_koid(handle, &koid));
+
+        char name[ZX_MAX_NAME_LEN];
+        RETURN_IF_ERROR(
+            zx_object_get_property(handle, ZX_PROP_NAME, name, sizeof(name)));
+
+        fbl::AllocChecker ac;
+        out->reset(new (&ac) RankedJob(koid, name));
+        if (!ac.check()) {
+            return ZX_ERR_NO_MEMORY;
+        }
+        return ZX_OK;
+    }
+
+    zx_koid_t koid() const { return koid_; }
+    const char* name() const { return name_; }
+
+    // Type of linked lists of this class.
+    using List = fbl::DoublyLinkedList<fbl::unique_ptr<RankedJob>>;
+
+private:
+    RankedJob(zx_koid_t koid, const char* name)
+        : koid_(koid) {
+        strlcpy(const_cast<char*>(name_), name, sizeof(name_));
+    }
+
+    const char name_[ZX_MAX_NAME_LEN] = {};
+    const zx_koid_t koid_;
+};
+
+RankedJob::List ranked_jobs;
+
+RankedJob* get_ranked_job_by_koid(zx_koid_t koid) {
+    for (auto& job : ranked_jobs) {
+        if (job.koid() == koid) {
+            return &job;
+        }
+    }
+    return nullptr;
+}
+
+} // namespace
+
+zx_status_t _fake_job_set_relative_importance(
+    zx_handle_t root_resource,
+    zx_handle_t job, zx_handle_t less_important_job) {
+
+    // Make sure the root resource looks legit.
+    zx_info_handle_basic_t info;
+    RETURN_IF_ERROR(zx_object_get_info(root_resource, ZX_INFO_HANDLE_BASIC,
+                                       &info, sizeof(info), nullptr, nullptr));
+    if (info.type != ZX_OBJ_TYPE_RESOURCE) {
+        return ZX_ERR_WRONG_TYPE;
+    }
+
+    unique_ptr<RankedJob> rjob;
+    {
+        zx_koid_t koid;
+        RETURN_IF_ERROR(get_handle_koid(job, &koid));
+        RankedJob* j = get_ranked_job_by_koid(koid);
+        if (j == nullptr) {
+            RETURN_IF_ERROR(RankedJob::Create(job, &rjob));
+        } else {
+            rjob.reset(ranked_jobs.erase(*j).release());
+        }
+    }
+    fflush(stdout);
+    ZX_ASSERT(!rjob->InContainer());
+
+    if (less_important_job == ZX_HANDLE_INVALID) {
+        // Make this the least important.
+        ranked_jobs.push_front(fbl::move(rjob));
+    } else {
+        // Insert rjob just after less_important_job.
+        zx_koid_t koid;
+        RETURN_IF_ERROR(get_handle_koid(less_important_job, &koid));
+        RankedJob* li_job = get_ranked_job_by_koid(koid);
+        // Simplification: less_important_job must exist already. The real
+        // syscall wouldn't have this restriction.
+        ZX_DEBUG_ASSERT(li_job != nullptr);
+        ranked_jobs.insert_after(
+            ranked_jobs.make_iterator(*li_job), fbl::move(rjob));
+    }
+    return ZX_OK;
+}
+
+void dump_importance_list() {
+    printf("Least important:\n");
+    for (const auto& rj : ranked_jobs) {
+        printf("- k:%" PRIu64 " [%-*s]\n",
+               rj.koid(), ZX_MAX_NAME_LEN, rj.name());
+    }
+}

system/uapp/oom/job.h
@@ -1,0 +1,100 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include <zircon/types.h>
+#include <fbl/intrusive_double_list.h>
+#include <fbl/unique_ptr.h>
+
+// A running Zircon job, with an open handle and cached properties.
+//
+// Defines a globally-sortable "importance key" that can be used to
+// rank all jobs by relative importance.
+//
+// Importance keys have the form:
+//
+//   <capped-importance> ':' <importance-chain> '~'
+//
+// <capped-importance> is the Job's capped_importance() as two hex digits.
+// <importance-chain> is the concatenation of the importance() values of a
+// Job's ancestors and itself, with the root-most Job's importance first.
+//
+// This produces a key that, when sorted using strcmp(), will cluster Jobs
+// with the same capped_importance(), then break ties using a Job's
+// ancestors' importances. The trailing '~' causes shorter strings to sort
+// higher than longer strings, ensuring that parents are more important
+// than their children.
+//
+// Example values:
+//
+//     32:c8c8c896c83264~ << A deep BACKGROUND job, capped to DISPOSABLE
+//     32:c8c8c896c832~
+//     32:c8c8c896c89632~
+//     64:c8c8c896c86464~
+//     64:c8c8c896c864c8~
+//     96:c8c8c896c8~     << A CRITICAL job, capped to FOREGROUND
+//     96:c8c8c896~       << The previous job's FOREGROUND parent
+//     96:c8c8c8c896~
+//     c8:c8c8~
+//     c8:c8~
+//
+// TODO(dbort): Consider using a similarly-structured uint64_t instead of a
+// string.
+class Job : public fbl::DoublyLinkedListable<fbl::unique_ptr<Job>> {
+public:
+    using importance_t = uint32_t;
+
+    // Takes ownership of |handle|, even on failure. To aid testability, this
+    // class must not perform any syscalls on |handle| except zx_handle_close.
+    static zx_status_t Create(
+        zx_koid_t koid, zx_handle_t handle,
+        const char* name, importance_t importance,
+        const Job* parent, fbl::unique_ptr<Job>* out);
+
+    ~Job();
+
+    zx_koid_t koid() const { return koid_; }
+    const char* name() const { return name_.get(); }
+
+    // The importance returned by ZX_PROP_JOB_IMPORTANCE.
+    importance_t importance() const { return importance_; }
+
+    // Like importance(), but capped to be no more important
+    // than any ancestor.
+    importance_t capped_importance() const {
+        return capped_importance_;
+    }
+
+    // An opaque, strcmp()-comparable string for sorting Jobs by their global
+    // relative importance. Higher importance will sort higher.
+    // See the class comment for a description of the keys.
+    const char* importance_key() const { return importance_key_.get(); }
+
+    // Returns the handle but maintains ownership.
+    zx_handle_t handle() const { return handle_; }
+
+    // Type for linked lists of this class.
+    using List = fbl::DoublyLinkedList<fbl::unique_ptr<Job>>;
+
+private:
+    Job(zx_koid_t koid, zx_handle_t handle, fbl::unique_ptr<char[]> name,
+        importance_t importance, importance_t capped_importance,
+        fbl::unique_ptr<char[]> importance_key);
+
+    const zx_koid_t koid_;
+
+    // An open handle to the underlying job object, or ZX_HANDLE_INVALID.
+    // Closed when this object is destroyed.
+    const zx_handle_t handle_;
+
+    const fbl::unique_ptr<char[]> name_;
+    const importance_t importance_;
+    const importance_t capped_importance_;
+    const fbl::unique_ptr<char[]> importance_key_;
+};
+
+// Reorders |jobs| so entries are ordered by importance_key(), with
+// the least-important job first.
+void sort_jobs_by_importance_key(Job::List *jobs);

system/uapp/oom/macros.h
@@ -1,0 +1,19 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include <zircon/status.h>
+#include <stdio.h>
+
+#define RETURN_IF_ERROR(x)                                             \
+    do {                                                               \
+        zx_status_t TRY_status__ = (x);                                \
+        if (TRY_status__ != ZX_OK) {                                   \
+            printf("%s:%d: %s failed: %s (%d)\n",                      \
+                    __func__, __LINE__, #x,                            \
+                    zx_status_get_string(TRY_status__), TRY_status__); \
+            return TRY_status__;                                       \
+        }                                                              \
+    } while (0)

system/uapp/oom/resources.h
@@ -1,0 +1,17 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include <zircon/compiler.h>
+#include <zircon/types.h>
+
+__BEGIN_CDECLS
+
+// Returns a new handle to the root resource, which the caller
+// is responsible for closing.
+// See docs/objects/resource.md
+zx_status_t get_root_resource(zx_handle_t* root_resource);
+
+__END_CDECLS

system/uapp/oom/rules.mk
@@ -1,0 +1,34 @@
+# Copyright 2017 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+MODULE := $(LOCAL_DIR).ranker
+
+MODULE_NAME := oom-ranker
+
+MODULE_TYPE := userapp
+
+MODULE_SRCS += \
+    $(LOCAL_DIR)/canned_jobs.cpp \
+    $(LOCAL_DIR)/job.cpp \
+    $(LOCAL_DIR)/ranker.cpp \
+    $(LOCAL_DIR)/resources.c
+
+# Tests
+MODULE_SRCS += \
+    $(LOCAL_DIR)/fake_syscalls.cpp
+
+MODULE_STATIC_LIBS := \
+    system/ulib/task-utils \
+    system/ulib/zxcpp \
+    system/ulib/zx \
+    system/ulib/fbl
+
+MODULE_LIBS := \
+    system/ulib/fdio \
+    system/ulib/zircon \
+    system/ulib/c
+
+include make/module.mk