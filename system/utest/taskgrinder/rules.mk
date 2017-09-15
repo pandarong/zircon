zhuzhengchao/magenta

WIP(taskgrinder): tool that creates/mutates/kills a bunch of tasks
Change-Id: Id172f36e6057d608df77b805f72887f209238ad1

system/utest/taskgrinder/rules.mk
@@ -1,0 +1,26 @@
+# Copyright 2017 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+MODULE := $(LOCAL_DIR)
+
+MODULE_TYPE := userapp
+
+MODULE_SRCS += \
+    $(LOCAL_DIR)/taskgrinder.cpp
+
+MODULE_NAME := taskgrinder
+
+MODULE_LIBS := \
+    system/ulib/mxio \
+    system/ulib/magenta \
+    system/ulib/c
+
+MODULE_STATIC_LIBS := \
+    system/ulib/mx \
+    system/ulib/mxtl \
+    system/ulib/mxcpp
+
+include make/module.mk

system/utest/taskgrinder/vector.h
@@ -1,0 +1,55 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include <mxtl/array.h>
+#include <mxtl/type_support.h>
+
+// A simple subset of std::vector.
+template <typename T>
+class Vector : private mxtl::Array<T> {
+private:
+    static constexpr size_t kInitialCapacity = 8;
+    typedef mxtl::Array<T> arrt;
+
+public:
+    Vector()
+        : arrt(Alloc(kInitialCapacity), kInitialCapacity) {
+    }
+
+    void push_back(T&& val) {
+        if (size() == size_) {
+            const size_t new_size = (size_ + 1) * 2;
+            // TODO(dbort): To do this properly, we'd need to avoid
+            // constructing any elements beyond size_.
+            T* a = Alloc(new_size);
+            for (size_t i = 0; i < size_; i++) {
+                a[i] = mxtl::move(arrt::operator[](i));
+            }
+            arrt::reset(a, new_size);
+        }
+        arrt::operator[](size_++) = mxtl::move(val);
+    }
+
+    T& operator[](size_t i) const {
+        MX_DEBUG_ASSERT(i < size_);
+        return arrt::operator[](i);
+    }
+
+    using arrt::begin;
+
+    T* end() const { return begin() + size_; }
+
+    size_t size() const { return size_; }
+
+    bool empty() const { return size_ == 0; }
+
+private:
+    T* Alloc(size_t size) {
+        return new T[size];
+    }
+
+    size_t size_ = 0;
+};

pandarong/zircon

system/utest/taskgrinder/rules.mk
@@ -1,0 +1,26 @@
+# Copyright 2017 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+MODULE := $(LOCAL_DIR)
+
+MODULE_TYPE := userapp
+
+MODULE_SRCS += \
+    $(LOCAL_DIR)/taskgrinder.cpp
+
+MODULE_NAME := taskgrinder
+
+MODULE_LIBS := \
+    system/ulib/fdio \
+    system/ulib/zircon \
+    system/ulib/c
+
+MODULE_STATIC_LIBS := \
+    system/ulib/zx \
+    system/ulib/fbl \
+    system/ulib/zxcpp
+
+include make/module.mk

system/utest/taskgrinder/vector.h
@@ -1,0 +1,55 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include <fbl/array.h>
+#include <fbl/type_support.h>
+
+// A simple subset of std::vector.
+template <typename T>
+class Vector : private fbl::Array<T> {
+private:
+    static constexpr size_t kInitialCapacity = 8;
+    typedef fbl::Array<T> arrt;
+
+public:
+    Vector()
+        : arrt(Alloc(kInitialCapacity), kInitialCapacity) {
+    }
+
+    void push_back(T&& val) {
+        if (size() == size_) {
+            const size_t new_size = (size_ + 1) * 2;
+            // TODO(dbort): To do this properly, we'd need to avoid
+            // constructing any elements beyond size_.
+            T* a = Alloc(new_size);
+            for (size_t i = 0; i < size_; i++) {
+                a[i] = fbl::move(arrt::operator[](i));
+            }
+            arrt::reset(a, new_size);
+        }
+        arrt::operator[](size_++) = fbl::move(val);
+    }
+
+    T& operator[](size_t i) const {
+        ZX_DEBUG_ASSERT(i < size_);
+        return arrt::operator[](i);
+    }
+
+    using arrt::begin;
+
+    T* end() const { return begin() + size_; }
+
+    size_t size() const { return size_; }
+
+    bool empty() const { return size_ == 0; }
+
+private:
+    T* Alloc(size_t size) {
+        return new T[size];
+    }
+
+    size_t size_ = 0;
+};