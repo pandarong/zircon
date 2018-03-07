db23022a127178735249362e29f9605e30a4a2d8
Mike Voydanoff <voydanoff@google.com>
2018-03-07 10:31:58 -0800

    [dev][usb][xhci] Use uncached io_buffers and eliminate cache flushing/invalidating
    Change-Id: Ie7e2a6c8761fc624c2c5e143636ffc71cec25aad

--- system/dev/usb/xhci/xhci.c
+++ system/dev/usb/xhci/xhci.c
@@ -214,12 +214,14 @@ zx_status_t xhci_init(xhci_t* xhci, xhci_mode_t mode, uint32_t num_interrupts) {
     }
 
     // Allocate DMA memory for various things
-    result = io_buffer_init(&xhci->dcbaa_erst_buffer, PAGE_SIZE, IO_BUFFER_RW | IO_BUFFER_CONTIG);
+    result = io_buffer_init(&xhci->dcbaa_erst_buffer, PAGE_SIZE,
+                            IO_BUFFER_RW | IO_BUFFER_CONTIG | IO_BUFFER_UNCACHED);
     if (result != ZX_OK) {
         zxlogf(ERROR, "io_buffer_init failed for xhci->dcbaa_erst_buffer\n");
         goto fail;
     }
-    result = io_buffer_init(&xhci->input_context_buffer, PAGE_SIZE, IO_BUFFER_RW | IO_BUFFER_CONTIG);
+    result = io_buffer_init(&xhci->input_context_buffer, PAGE_SIZE,
+                            IO_BUFFER_RW | IO_BUFFER_CONTIG | IO_BUFFER_UNCACHED);
     if (result != ZX_OK) {
         zxlogf(ERROR, "io_buffer_init failed for xhci->input_context_buffer\n");
         goto fail;
@@ -237,7 +239,7 @@ zx_status_t xhci_init(xhci_t* xhci, xhci_mode_t mode, uint32_t num_interrupts) {
         }
         size_t scratch_pad_index_size = PAGE_ROUNDUP(scratch_pad_bufs * sizeof(uint64_t));
         result = io_buffer_init(&xhci->scratch_pad_index_buffer, scratch_pad_index_size,
-                                IO_BUFFER_RW | IO_BUFFER_CONTIG);
+                                IO_BUFFER_RW | IO_BUFFER_CONTIG | IO_BUFFER_UNCACHED);
         if (result != ZX_OK) {
             zxlogf(ERROR, "io_buffer_init failed for xhci->scratch_pad_index_buffer\n");
             goto fail;
@@ -340,7 +342,6 @@ uint32_t xhci_get_max_interrupters(xhci_t* xhci) {
 }
 
 int xhci_get_slot_ctx_state(xhci_slot_t* slot) {
-    xhci_cache_flush_invalidate(&slot->sc->sc3, sizeof(slot->sc->sc3));
     return XHCI_GET_BITS32(&slot->sc->sc3, SLOT_CTX_SLOT_STATE_START,
                            SLOT_CTX_CONTEXT_ENTRIES_BITS);
 }
@@ -349,7 +350,6 @@ int xhci_get_ep_ctx_state(xhci_slot_t* slot, xhci_endpoint_t* ep) {
     if (!ep->epc) {
         return EP_CTX_STATE_DISABLED;
     }
-    xhci_cache_flush_invalidate(&ep->epc->epc0, sizeof(ep->epc->epc0));
     return XHCI_GET_BITS32(&ep->epc->epc0, EP_CTX_EP_STATE_START, EP_CTX_EP_STATE_BITS);
 }
 
@@ -391,7 +391,6 @@ void xhci_wait_bits64(volatile uint64_t* ptr, uint64_t bits, uint64_t expected) 
 
 void xhci_set_dbcaa(xhci_t* xhci, uint32_t slot_id, zx_paddr_t paddr) {
     XHCI_WRITE64(&xhci->dcbaa[slot_id], paddr);
-    xhci_cache_flush(&xhci->dcbaa[slot_id], sizeof(xhci->dcbaa[slot_id]));
 }
 
 zx_status_t xhci_start(xhci_t* xhci) {
@@ -584,9 +583,6 @@ uint64_t xhci_get_current_frame(xhci_t* xhci) {
 
 static void xhci_handle_events(xhci_t* xhci, int interrupter) {
     xhci_event_ring_t* er = &xhci->event_rings[interrupter];
-
-    // invalidate event ring before processing new events
-    xhci_cache_flush_invalidate(er->start, er->buffer.size);
 
     // process all TRBs with cycle bit matching our CCS
     while ((XHCI_READ32(&er->current->control) & TRB_C) == er->ccs) {
