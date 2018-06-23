2c129668eba657fbb5b1bc21eb3b28d867a1b4c4
Mike Voydanoff <voydanoff@google.com>
2018-06-22 20:52:04 -0700

    astro printery
    Change-Id: Iada3b734a0bc825f6e2d1ff2916bd632dff288e4

--- system/dev/lib/amlogic/aml-usb-phy-v2.c
+++ system/dev/lib/amlogic/aml-usb-phy-v2.c
@@ -8,6 +8,8 @@
 #include <soc/aml-common/aml-usb-phy-v2-regs.h>
 #include <soc/aml-s905d2/s905d2-hw.h>
 
+#include <unistd.h>
+
 // from mesong12a.dtsi
 #define PLL_SETTING_0   0x09400414
 #define PLL_SETTING_1   0x927E0000
@@ -18,22 +20,66 @@ static zx_status_t set_usb_pll(zx_paddr_t reg_base, zx_handle_t bti) {
     io_buffer_t buf;
     zx_status_t status;
 
+printf("set_usb_pll\n");
     status = io_buffer_init_physical(&buf, bti, reg_base, PAGE_SIZE, get_root_resource(),
                                      ZX_CACHE_POLICY_UNCACHED_DEVICE);
     if (status != ZX_OK) {
         return status;
     }
     void* reg = io_buffer_virt(&buf);
+printf("pll regs: %p phys %p\n", reg, (void*)io_buffer_phys(&buf));
 
+printf("pll write %08x to %p\n", (0x30000000 | PLL_SETTING_0), reg + 0x40);
     writel((0x30000000 | PLL_SETTING_0), reg + 0x40);
+printf("pll write %08x to %p\n", PLL_SETTING_1, reg + 0x44);
     writel(PLL_SETTING_1, reg + 0x44);
+printf("pll write %08x to %p\n", PLL_SETTING_2, reg + 0x48);
     writel(PLL_SETTING_2, reg + 0x48);
     zx_nanosleep(zx_deadline_after(ZX_USEC(100)));
+printf("pll write %08x to %p\n", (0x10000000 | PLL_SETTING_0), reg + 0x40);
     writel((0x10000000 | PLL_SETTING_0), reg + 0x40);
 
     io_buffer_release(&buf);
     return ZX_OK;
 }
+
+static void set_mode(volatile void* usbctrl_regs, bool host) {
+    // phy-aml-new-usb3-v2.c set_mode()
+    volatile void* u2p_r0 = usbctrl_regs + (1 * PHY_REGISTER_SIZE) + U2P_R0_OFFSET;
+    volatile void* usb_r0 = usbctrl_regs + USB_R0_OFFSET;
+    volatile void* usb_r4 = usbctrl_regs + USB_R4_OFFSET;
+    uint32_t temp;
+
+    temp = readl(usb_r0);
+    if (host) {
+        temp &= ~USB_R0_U2D_ACT;
+    } else {
+        temp |= USB_R0_U2D_ACT;
+        temp &= ~USB_R0_U2D_SS_SCALEDOWN_MODE;
+    }
+printf("USB_R0_U2D_ACT etc write %08x to %p\n", temp, usb_r0);
+    writel(temp, usb_r0);
+
+    temp = readl(usb_r4);
+    if (host) {
+        temp &= ~USB_R4_P21_SLEEPM0;
+    } else {
+        temp |= USB_R4_P21_SLEEPM0;
+    }
+printf("USB_R4_P21_SLEEPM0 write %08x to %p\n", temp, usb_r4);
+    writel(temp, usb_r4);
+
+    temp = readl(u2p_r0);
+    if (host) {
+        temp |= U2P_R0_HOST_DEVICE;
+    } else {
+        temp &= ~U2P_R0_HOST_DEVICE;
+    }
+    temp &= ~U2P_R0_POR;
+printf("u2p_r0 write %08x to %p\n", temp, u2p_r0);
+    writel(temp, u2p_r0);
+}
+
 
 zx_status_t aml_usb_phy_v2_init(zx_handle_t bti, bool host) {
     zx_status_t status;
@@ -41,6 +87,7 @@ zx_status_t aml_usb_phy_v2_init(zx_handle_t bti, bool host) {
     io_buffer_t usbctrl_buf;
     uint32_t temp;
 
+printf("aml_usb_phy_v2_init\n");
     status = io_buffer_init_physical(&reset_buf, bti, S905D2_RESET_BASE, S905D2_RESET_LENGTH,
                                      get_root_resource(),
                                      ZX_CACHE_POLICY_UNCACHED_DEVICE);
@@ -61,8 +108,12 @@ zx_status_t aml_usb_phy_v2_init(zx_handle_t bti, bool host) {
     volatile void* reset_regs = io_buffer_virt(&reset_buf);
     volatile void* usbctrl_regs = io_buffer_virt(&usbctrl_buf);
 
+printf("reset_regs: %p phys %p\n", reset_regs, (void*)io_buffer_phys(&reset_buf));
+printf("usbctrl_regs: %p phys %p\n", usbctrl_regs, (void*)io_buffer_phys(&usbctrl_buf));
+
     // first reset USB
     uint32_t val = readl(reset_regs + 0x21 * 4);
+printf("reset write %08x to %p\n", (val | (0x3 << 16)), reset_regs + 0x21 * 4);
     writel((val | (0x3 << 16)), reset_regs + 0x21 * 4);
 
     // amlogic_new_usbphy_reset_v2()
@@ -81,11 +132,13 @@ zx_status_t aml_usb_phy_v2_init(zx_handle_t bti, bool host) {
             temp |= U2P_R0_IDPULLUP0;
             temp |= U2P_R0_DRVVBUS0;
         }
+printf("i: %d por etc write %08x to %p\n", i, temp, addr);
         writel(temp, addr);
 
         zx_nanosleep(zx_deadline_after(ZX_USEC(10)));
 
         // amlogic_new_usbphy_reset_phycfg_v2()
+printf("i: %d reset_1 write %08x to %p\n", i, readl(reset_1) | (1 << (16 + 0 /*i is always zero here */)), reset_1);
         writel(readl(reset_1) | (1 << (16 + 0 /*i is always zero here */)), reset_1);
 
         zx_nanosleep(zx_deadline_after(ZX_USEC(50)));
@@ -113,36 +166,9 @@ zx_status_t aml_usb_phy_v2_init(zx_handle_t bti, bool host) {
         zxlogf(ERROR, "aml_usb_init: set_usb_pll failed: %d\n", status);
     }
 
-    // phy-aml-new-usb3-v2.c set_mode()
-    volatile void* u2p_r0 = usbctrl_regs + (1 * PHY_REGISTER_SIZE) + U2P_R0_OFFSET;
-    volatile void* usb_r0 = usbctrl_regs + USB_R0_OFFSET;
-    volatile void* usb_r4 = usbctrl_regs + USB_R4_OFFSET;
-
-    temp = readl(usb_r0);
-    if (host) {
-        temp &= ~USB_R0_U2D_ACT;
-    } else {
-        temp |= USB_R0_U2D_ACT;
-        temp &= ~USB_R0_U2D_SS_SCALEDOWN_MODE;
-    }
-    writel(temp, usb_r0);
-
-    temp = readl(usb_r4);
-    if (host) {
-        temp &= ~USB_R4_P21_SLEEPM0;
-    } else {
-        temp |= USB_R4_P21_SLEEPM0;
-    }
-    writel(temp, usb_r4);
-
-    temp = readl(u2p_r0);
-    if (host) {
-        temp |= U2P_R0_HOST_DEVICE;
-    } else {
-        temp &= ~U2P_R0_HOST_DEVICE;
-    }
-    temp &= ~U2P_R0_POR;
-    writel(temp, u2p_r0);
+    set_mode(usbctrl_regs, true);
+    sleep(1);
+    set_mode(usbctrl_regs, false);
 
     io_buffer_release(&reset_buf);
     io_buffer_release(&usbctrl_buf);
