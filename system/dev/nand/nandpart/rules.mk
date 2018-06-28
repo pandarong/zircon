a8765dbfb90aad5d7fb8ca05879eb5a9d691907f
Suraj Malhotra <surajmalhotra@google.com>
2018-06-28 16:06:54 -0700

    [nandpart] Rewrite nandpart in C++
    Change-Id: Id67e223706ff09c90392483922d5fcc668504bb4
    Tested: Manually testd that enumeration works via `dm dump`

--- system/dev/nand/nandpart/rules.mk
+++ system/dev/nand/nandpart/rules.mk
@@ -8,10 +8,21 @@ MODULE := $(LOCAL_DIR)
 
 MODULE_TYPE := driver
 
-MODULE_SRCS := $(LOCAL_DIR)/nandpart.c
+MODULE_SRCS := \
+    $(LOCAL_DIR)/binding.c \
+    $(LOCAL_DIR)/nandpart.cpp \
 
-MODULE_STATIC_LIBS := system/ulib/ddk system/ulib/sync third_party/ulib/cksum
+MODULE_STATIC_LIBS := \
+    system/ulib/ddk \
+    system/ulib/ddktl \
+    system/ulib/fbl \
+    system/ulib/sync \
+    system/ulib/zx \
+    system/ulib/zxcpp \
 
-MODULE_LIBS := system/ulib/driver system/ulib/zircon system/ulib/c
+MODULE_LIBS := \
+    system/ulib/c \
+    system/ulib/driver \
+    system/ulib/zircon \
 
 include make/module.mk
